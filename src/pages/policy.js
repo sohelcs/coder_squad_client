2f980473d738f352215ec88a71b8917885b39967
AfnanFerdousi <afnanferdousi550@gmail.com>
2024-03-25 02:42:18 +0600

    update

---
+++ src/pages/policy.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '../components/shared/Navbar';
+import Privacy from '../components/privacy/Privacy';
+import Footer from '../components/shared/Footer';
+
+const Policy = () => {
+    return (
+        <div>
+            <Head>
+                <title>Privacy Policy</title>
+            </Head>
+            <Navbar/>
+            <Privacy/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Policy;
+
+// fdfjksgsf
